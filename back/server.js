de5b328713fba5d2b23f36c2eaa4d45c5765d142
ThomasGrare <thomasgrare64@gmail.com>
2023-10-05 23:08:50 +0200

    init back project

--- back/server.js
+++ back/server.js
@@ -1,23 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Serveur REST fonctionne !');
-});
+// ...
 
-const tasks = ["test"];
+// Utilisation du routeur Express pour la route
 
-app.get('/tasks', (req, res) => {
-  res.json(tasks);
-});
 
-app.post('/tasks', (req, res) => {
-  const newTask = req.body; // Vous devez utiliser un middleware pour analyser le corps JSON de la demande
-  tasks.push(newTask);
-  res.status(201).json(newTask);
-});
+// ...
 
+// Lancez le serveur Express
+const port = 3000; // Port sur lequel le serveur écoutera
 app.listen(port, () => {
   console.log(`Serveur en cours d'exécution sur le port ${port}`);
 });
