a6cce737a56604acc7a9a2784cb42eb1f6d18681
Rémy <remy.berjonneau@gmail.com>
2023-10-06 16:23:56 +0200

    popup

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Menu from "@/components/Menu/MenuComponent.vue";
-import Data from "@/components/Data/DataComponent.vue"
 </script>
 
 <template>
@@ -9,6 +8,5 @@ import Data from "@/components/Data/DataComponent.vue"
     <!-- Composant side bar -->
     <RouterView />
     <Menu></Menu>
-    <Data></Data>
   </div>
 </template>
--- front/src/components/Map/MapComponent.vue
+++ front/src/components/Map/MapComponent.vue
@@ -15,10 +15,10 @@ export default {
     };
   },
   mounted() {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        this.map = L.map("mapContainer").setView([latitude, longitude], 18);
+    // navigator.geolocation.getCurrentPosition(
+    //   (position) => {
+    //     const { latitude, longitude } = position.coords;
+        this.map = L.map("mapContainer").setView([48.8566, 2.3522], 18);
 
         L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
           attribution:
@@ -26,7 +26,7 @@ export default {
         }).addTo(this.map);
 
         this.loadJsonData();
-      },
+      //},
       (error) => {
         console.error("Erreur lors de la géolocalisation : ", error);
         this.map = L.map("mapContainer").setView([48.8566, 2.3522], 18);
@@ -38,7 +38,7 @@ export default {
 
         this.loadJsonData();
       }
-    );
+    //);
   },
   methods: {
     loadJsonData() {
@@ -52,18 +52,38 @@ export default {
         .then((data) => {
           this.jsonData = data;
           this.addMarkers();
+          console.log(this.jsonData[0])
         })
         .catch((error) => {
           console.error("Erreur lors du chargement des données JSON", error);
         });
+    },
+    formatAddress(address) {
+      // Réorganise l'adresse au format "QUAI DE GESVRES"
+      const parts = address.split(" (");
+      if (parts.length > 1) {
+        return `${parts[1].replace(")", "")} ${parts[0]}`;
+      }
+      return address;
     },
     addMarkers() {
       const visibleBounds = this.map.getBounds();
 
       this.jsonData.forEach((item) => {
         if (visibleBounds.contains(L.latLng(item.geo_point_2d.lat, item.geo_point_2d.lon))) {
-          const marker = L.marker([item.geo_point_2d.lat, item.geo_point_2d.lon]);
-          marker.addTo(this.map);
+          const formattedAddress = this.formatAddress(item.lib_voie);
+          L.marker([item.geo_point_2d.lat, item.geo_point_2d.lon])
+            .addTo(this.map)
+            .bindPopup(`
+              <div>
+                <p>ID: ${item.cod_lampe}</p>
+                <p>Arrondissement: ${item.lib_region}</p>
+                <p>Adresse: ${formattedAddress}</p>
+                <router-link :to="http://localhost:5173//report">
+                <button class="popup-button">Rediriger vers la page de signalement</button>
+              </router-link>
+              </div>
+            `);
         }
       });
     },
@@ -86,3 +106,123 @@ export default {
   z-index: 1;
 }
 </style>
+
+
+<!-- <template>
+  <div id="mapContainer"></div>
+    <div id="popup">
+        <h3>Lampe publique</h3>
+        <table>
+          <tr>
+            <td>ID</td>
+            <td>${item.cod_lampe}</td>
+          </tr>
+          <tr>
+            <td>Arrondissement</td>
+            <td>${item.lib_region}</td>
+          </tr>
+          <tr>
+            <td>Adresse</td>
+            <td>${formattedAddress}</td>
+          </tr>
+        </table>
+        <router-link to"localhost:5173/report">
+          <button class="btn">Rediriger vers la page de signalement</button>
+        </a>
+    </div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+
+
+export default {
+  name: "LeafletMap",
+  components: {
+
+  },
+  data() {
+    return {
+      jsonData: [],
+      map: null,
+    };
+  },
+  mounted() {
+    this.map = L.map("mapContainer").setView([48.8566, 2.3522], 18);
+
+    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+      attribution:
+        "&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors",
+    }).addTo(this.map);
+
+    this.loadJsonData();
+  },
+  methods: {
+    loadJsonData() {
+      fetch("/eclairage-public.json")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Erreur réseau");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          this.jsonData = data;
+          this.addMarkers();
+          console.log(this.jsonData[0]);
+        })
+        .catch((error) => {
+          console.error("Erreur lors du chargement des données JSON", error);
+        });
+    },
+    formatAddress(address) {
+      const parts = address.split(" (");
+      if (parts.length > 1) {
+        return `${parts[1].replace(")", "")} ${parts[0]}`;
+      }
+      return address;
+    },
+    addMarkers() {
+      const visibleBounds = this.map.getBounds();
+
+      this.jsonData.forEach((item) => {
+        if (visibleBounds.contains(L.latLng(item.geo_point_2d.lat, item.geo_point_2d.lon))) {
+          const formattedAddress = this.formatAddress(item.lib_voie);
+          L.marker([item.geo_point_2d.lat, item.geo_point_2d.lon])
+            .addTo(this.map)
+            .bindPopup(`
+              <div id="popupContent">
+                <h3>Lampe publique</h3>
+                <table>
+                  <tr>
+                    <td>ID</td>
+                    <td>${item.cod_lampe}</td>
+                  </tr>
+                  <tr>
+                    <td>Arrondissement</td>
+                    <td>${item.lib_region}</td>
+                  </tr>
+                  <tr>
+                    <td>Adresse</td>
+                    <td>${formattedAddress}</td>
+                  </tr>
+                </table>
+                <a href="localhost:5173/report">
+                  <button class="btn">Rediriger vers la page de signalement</button>
+                </a>
+              </div>
+            `);
+        }
+      });
+    },
+  },
+  onBeforeUnmount() {
+    if (this.map) {
+      this.map.remove();
+    }
+},
+};
+</script>
+ -->
